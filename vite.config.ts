e96441418a9524a4044702af8d777ead8f43f8fd
RAZdevs <rami.abuzaid93@gmail.com>
2024-10-31 18:49:01 +0100

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',  // This will be your backend server
+        target: 'portfolio1-production-f876.up.railway.app',  // This will be your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
