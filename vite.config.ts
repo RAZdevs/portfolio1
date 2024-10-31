cf97d88da74189849b8b5ddaa5d8508806075c26
RAZdevs <rami.abuzaid93@gmail.com>
2024-10-31 18:33:15 +0100

    api update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',  // This will be your backend server
+        target: 'http://localhost:3000',  // This will be your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
